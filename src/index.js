d31de96bed2dfdb8f5e61cb978779a5a303c093a
Sreeram <rachasreeram@gmail.com>
2022-11-01 15:59:31 +0530

    Added git ignore file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AppProvider } from "./components/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<AppProvider>
-			<App />
-		</AppProvider>
+		<App />
 	</React.StrictMode>
 );
